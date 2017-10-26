f0ddb1f40e49151a4219c8071341c73ca5fdc459
Yukai Huang <yukaihuangtw@gmail.com>
2017-10-26 13:48:34 +0800

    Lint with standard

--- bin/build.js
+++ bin/build.js
@@ -12,13 +12,15 @@ require('dotenv').config()
 
 const BASE_URL = 'https://www.nstc.org.tw/'
 
-Date.prototype.addDays = function(days) {
-  var dat = new Date(this.valueOf());
-  dat.setDate(dat.getDate() + days);
-  return dat;
+/* eslint-disable no-extend-native */
+Date.prototype.addDays = function (days) {
+  var dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days)
+  return dat
 }
+/* eslint-enable no-extend-native */
 
-function main() {
+function main () {
   rp(BASE_URL).then(html => cheerio.load(html)).then(async function ($) {
     const href = $('#divSystemFoodMenu a').attr('href')
     const pdfUrl = url.resolve(BASE_URL, href)
@@ -26,11 +28,11 @@ function main() {
     const xlsPath = tempfile('.xls')
 
     // 20170928093553178
-    var matchData = $('#divSystemFoodMenu a').attr('title').match(/(\d{4})\-(\d{2})\-(\d{2})/)
+    var matchData = $('#divSystemFoodMenu a').attr('title').match(/(\d{4})-(\d{2})-(\d{2})/)
 
     await rp(pdfUrl, { encoding: 'binary' }).then(res => {
-      const buffer = Buffer.from(res, 'binary');
-      fs.writeFileSync(downloadPath, buffer, 'binary');
+      const buffer = Buffer.from(res, 'binary')
+      fs.writeFileSync(downloadPath, buffer, 'binary')
 
       var formData = {
         tab2exkey: process.env.API_KEY,
@@ -39,15 +41,15 @@ function main() {
         outputFormat: 'XLS',
         xlsExportType: 'xlsx',
         file: fs.createReadStream(downloadPath)
-      };
+      }
       rp.post({
         url: 'http://api2.pdfextractoronline.com:8089/tab2ex2/api',
         encoding: 'binary',
         formData: formData
       }).then(res => {
-        const buffer = Buffer.from(res, 'binary');
-        fs.writeFileSync(xlsPath, buffer, 'binary');
-        const worksheets = xlsx.parse(xlsPath);
+        const buffer = Buffer.from(res, 'binary')
+        fs.writeFileSync(xlsPath, buffer, 'binary')
+        const worksheets = xlsx.parse(xlsPath)
 
         //                                              month is zero-based
         var uploadedDay = new Date(Date.UTC(matchData[1], matchData[2] - 1, matchData[3]))
@@ -59,11 +61,11 @@ function main() {
         let data = {}
         for (var i = 0; i < worksheets.length; i++) {
           var day = firstDay.addDays(i)
-          var dayString = day.toISOString().slice(0,10).replace(/-/g,"");
+          var dayString = day.toISOString().slice(0, 10).replace(/-/g, '')
 
-          function addMeals(index) {
+          const addMeals = (index) => {
             var meals = []
-            for (var mealIndex = 1; mealIndex < worksheets[i].data.length; mealIndex ++) {
+            for (var mealIndex = 1; mealIndex < worksheets[i].data.length; mealIndex++) {
               var meal = worksheets[i].data[mealIndex][index]
               if (meal && meal.length > 0) {
                 meals.push(meal)
